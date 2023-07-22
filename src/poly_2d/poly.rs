f09c820168a0e2c9565270ec50c0741302f5a3a5
Sebastian Göb <sebastiancgoeb@gmail.com>
2023-07-22 14:33:01 +0200

    remove unused code and apply clippy fixes

--- src/poly_2d/poly.rs
+++ src/poly_2d/poly.rs
@@ -1,13 +1,11 @@
 use std::hash::{Hash, Hasher};
 use std::{
-    cell::OnceCell,
     collections::{HashMap, HashSet},
     fmt::Display,
     time::Instant,
 };
 
 use itertools::Itertools;
-use ndarray::{Array2, Axis};
 
 use crate::cli::Poly2d;
 
@@ -76,33 +74,6 @@ fn generate_polys_of_size(
     new_polys
 }
 
-fn is_duplicate(polys: &[Shape], poly: &Shape) -> bool {
-    return polys.iter().any(|other| {
-        if other.dimensions() != poly.dimensions() {
-            return false;
-        }
-
-        if other.degrees() != poly.degrees() {
-            return false;
-        }
-
-        let mut rot90 = other.grid().t();
-        rot90.invert_axis(Axis(1));
-
-        let mut rot180 = other.grid().view();
-        rot180.invert_axis(Axis(0));
-        rot180.invert_axis(Axis(1));
-
-        let mut rot270 = other.grid().t();
-        rot270.invert_axis(Axis(0));
-
-        other.grid() == poly.grid()
-            || rot90 == poly.grid()
-            || rot180 == poly.grid()
-            || rot270 == poly.grid()
-    });
-}
-
 fn report_performance(start: Instant, tried: usize, found: usize) {
     let dur = start.elapsed();
 
@@ -144,7 +115,7 @@ impl Hash for BinShape {
     where
         H: Hasher,
     {
-        &self.grid.hash(state);
+        self.grid.hash(state);
     }
 }
 
@@ -191,11 +162,11 @@ impl BinShape {
         let idx: usize = vec![&grid_0, &grid_90, &grid_180, &grid_270]
             .iter()
             .enumerate()
-            .max_by(|(_, a), (_, b)| a.cmp(&b))
+            .max_by(|(_, a), (_, b)| a.cmp(b))
             .unwrap()
             .0;
 
-        return match idx {
+        match idx {
             0 => BinShape {
                 points,
                 grid: grid_0,
@@ -217,7 +188,7 @@ impl BinShape {
                 dimensions: dims_270,
             },
             _ => panic!(),
-        };
+        }
     }
 }
 
@@ -228,7 +199,7 @@ impl Display for BinShape {
                 let present = (row >> i_1) & 0x1 != 0;
                 write!(f, "{}", if present { 'O' } else { ' ' })?;
             }
-            write!(f, "\n")?;
+            writeln!(f)?;
         }
 
         Ok(())
@@ -255,112 +226,5 @@ impl Vec2 for (i32, i32) {
 
     fn translate(&self, t: (i32, i32)) -> Self {
         (self.0 + t.0, self.1 + t.1)
-    }
-}
-
-struct Shape {
-    points: Vec<(i32, i32)>,
-    grid: OnceCell<Array2<i32>>,
-    dimensions: OnceCell<Vec<usize>>,
-    degrees: OnceCell<Vec<usize>>,
-}
-
-impl Shape {
-    fn from(points: Vec<(i32, i32)>) -> Shape {
-        Shape {
-            points,
-            grid: OnceCell::new(),
-            dimensions: OnceCell::new(),
-            degrees: OnceCell::new(),
-        }
-    }
-
-    // TODO i32 overkill, can be bool or u8?
-    fn grid(&self) -> &Array2<i32> {
-        return self.grid.get_or_init(|| {
-            let min_x: i32 = self.points.iter().map(|p| p.0).min().unwrap();
-            let max_x: i32 = self.points.iter().map(|p| p.0).max().unwrap();
-            let min_y: i32 = self.points.iter().map(|p| p.1).min().unwrap();
-            let max_y: i32 = self.points.iter().map(|p| p.1).max().unwrap();
-            let w = max_x - min_x + 1;
-            let h = max_y - min_y + 1;
-
-            let mut grid = Array2::<i32>::zeros((w as usize, h as usize));
-            for p in &self.points {
-                grid[((p.0 - min_x) as usize, (p.1 - min_y) as usize)] = 1;
-            }
-
-            grid
-        });
-    }
-
-    fn dimensions(&self) -> &Vec<usize> {
-        return self.dimensions.get_or_init(|| {
-            let mut dims = self.grid().shape().to_vec();
-            dims.sort();
-            dims
-        });
-    }
-
-    fn degrees(&self) -> &Vec<usize> {
-        return self.degrees.get_or_init(|| {
-            let grid = self.grid();
-            let w = grid.len_of(Axis(0));
-            let h = grid.len_of(Axis(1));
-
-            let mut degrees: Vec<usize> = vec![0, 0, 0, 0, 0]; // degrees 0 to 4
-            for x in 0..w {
-                for y in 0..h {
-                    if grid[[x, y]] != 0 {
-                        let mut degree = 0;
-                        if x > 0 && grid[[x - 1, y]] != 0 {
-                            degree += 1;
-                        }
-                        if x < w - 1 && grid[[x + 1, y]] != 0 {
-                            degree += 1;
-                        }
-                        if y > 0 && grid[[x, y - 1]] != 0 {
-                            degree += 1;
-                        }
-                        if y < h - 1 && grid[[x, y + 1]] != 0 {
-                            degree += 1;
-                        }
-                        degrees[degree] += 1;
-                    }
-                }
-            }
-
-            degrees
-        });
-    }
-}
-
-impl PartialEq for Shape {
-    fn eq(&self, other: &Self) -> bool {
-        self.grid() == other.grid()
-    }
-}
-
-impl Display for Shape {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let [] = &self.points[..] {
-            write!(f, "This shape has no points")?;
-            return Ok(());
-        }
-
-        let min_x: i32 = self.points.iter().map(|p| p.0).min().unwrap();
-        let max_x: i32 = self.points.iter().map(|p| p.0).max().unwrap();
-        let min_y: i32 = self.points.iter().map(|p| p.1).min().unwrap();
-        let max_y: i32 = self.points.iter().map(|p| p.1).max().unwrap();
-        let w = max_x - min_x + 1;
-        let h = max_y - min_y + 1;
-
-        let mut grid = Array2::<i32>::zeros((w as usize, h as usize));
-        for (i, p) in self.points.iter().enumerate() {
-            grid[((p.0 - min_x) as usize, (p.1 - min_y) as usize)] = i as i32 + 1;
-        }
-        writeln!(f, "{}", grid)?;
-
-        Ok(())
     }
 }
